4b827f4de4cff769354845ae5c863cc753abba33
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2019-05-12 15:22:25 +0100

    Change from C++ to C.

---
+++ motor_control.c
@@ -0,0 +1,17 @@
+/* Library for controlling Dynamixel AX-12A motor using DynamixelSDK */
+/* Author: Liuchuyao Xu (Raymond) */
+
+#include "motor_control.h"
+
+int initialize()
+{
+
+  return 0;
+}
+
+int main ()
+{
+  initialize();
+
+  return 0;
+}
